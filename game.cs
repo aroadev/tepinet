ae9380db4c033794d9c87773cc297aca99fd7c21
hmnvkv <manrique.hamed1610@gmail.com>
2021-03-24 20:01:56 -0600

    JSON implementation

---
+++ game.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace tepinet
+{
+    public class game
+    {
+        public int GameName { get; set; }
+        public int VersionIO { get; set; }
+    }
+}
